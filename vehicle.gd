9ee94b80dfa72e979210ca3e4bbb1821a6a2b26a
Misteki <134941471+mistekii@users.noreply.github.com>
2025-06-06 02:15:47 +0100

    Update vehicle to use Wheel3D

--- vehicle.gd
+++ vehicle.gd
@@ -1,10 +1,6 @@
 extends RigidBody3D
 
-@export var wheels: Array[RayCast3D]
-@export var spring_strength := 100.0
-@export var spring_damping := 2.0
-@export var rest_dist := 0.5
-@export var wheel_radius := 0.4
+@export var wheels: Array[Wheel3D]
 
 func _physics_process(delta: float) -> void:
 	for wheel in wheels:
@@ -14,27 +10,27 @@ func _get_point_velocity(point: Vector3) -> Vector3:
 	return linear_velocity + angular_velocity.cross(point - global_position)
 
 # calculate & apply suspension forces
-func _do_single_wheel_suspension(suspension_ray: RayCast3D) -> void:
-	if suspension_ray.is_colliding():
+func _do_single_wheel_suspension(wheel: Wheel3D) -> void:
+	if wheel.is_colliding():
 		# ensure that the raycast will only collide when 
 		# when the wheel is *actually* touching the ground
-		suspension_ray.target_position.y = -(rest_dist + wheel_radius)
+		wheel.target_position.y = -(wheel.rest_dist + wheel.wheel_radius + wheel.over_extend)
 
-		var contact := suspension_ray.get_collision_point()
-		var spring_up_dir := suspension_ray.global_transform.basis.y
-		var spring_len := suspension_ray.global_position.distance_to(contact) - wheel_radius
-		var offset := rest_dist - spring_len
+		var contact := wheel.get_collision_point()
+		var spring_up_dir := wheel.global_transform.basis.y
+		var spring_len := wheel.global_position.distance_to(contact) - wheel.wheel_radius
+		var offset := wheel.rest_dist - spring_len
 
-		suspension_ray.get_node("mesh").position.y = -spring_len
+		wheel.mesh.position.y = -spring_len
 
-		var spring_force := spring_strength * offset
+		var spring_force := wheel.spring_strength * offset
 
 		var world_vel := _get_point_velocity(contact)
 		var relative_vel := spring_up_dir.dot(world_vel)
-		var spring_damp_force := spring_damping * relative_vel
+		var spring_damp_force := wheel.spring_damping * relative_vel
 
 		var force_vector := (spring_force - spring_damp_force) * spring_up_dir
 
 		var force_pos_offset := contact - global_position
-		
 		apply_force(force_vector, force_pos_offset)
+		DebugDraw3D.draw_arrow_ray(contact, force_vector / mass, 1, Color(1, 0, 0), 0.1)
