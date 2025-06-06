77eb4414a408c9d75af751ab2413951ed66ef26c
Misteki <134941471+mistekii@users.noreply.github.com>
2025-06-06 03:00:19 +0100

    Change default values for Wheel3D

--- wheel.gd
+++ wheel.gd
@@ -1,8 +1,8 @@
 extends RayCast3D
 class_name Wheel3D
 
-@export var spring_strength := 100.0
-@export var spring_damping := 2.0
+@export var spring_strength := 150.0
+@export var spring_damping := 4.0
 @export var rest_dist := 0.5
 @export var over_extend := 0.0 # used to control how much the wheel "sticks" to the ground
 @export var wheel_radius := 0.4
