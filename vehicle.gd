3ed09ea2413f2173e984129c0c2f56a2b0f2b4e0
Misteki <134941471+mistekii@users.noreply.github.com>
2025-06-06 03:40:35 +0100

    Add basic steering, traction and handbrake functionality

--- vehicle.gd
+++ vehicle.gd
@@ -2,29 +2,53 @@ extends RigidBody3D
 
 @export var wheels: Array[Wheel3D]
 @export var acceleration := 20.0
-@export var deceleration := 2.0
 @export var max_speed := 20
 @export var accel_curve: Curve
+@export var tire_turn_speed := 2.0
+@export var tire_max_turn_degrees := 25
 
 var motor_input := 0.0
+var handbrake := false
+var is_slipping := false
 
 func _unhandled_input(event: InputEvent) -> void:
+	handbrake = Input.is_action_pressed("handbrake")
+	
+	if Input.is_action_pressed("handbrake"):	
+		is_slipping = true
+	
+	
 	motor_input = Input.get_axis("decelerate", "accelerate")
 
 func _physics_process(delta: float) -> void:
+	_basic_steering_rotation(delta)
+	
 	for wheel in wheels:
 		wheel.force_raycast_update()
 		_do_single_wheel_suspension(wheel)
 		_do_single_wheel_acceleration(wheel)
+		_do_single_wheel_traction(wheel)
 
 func _get_point_velocity(point: Vector3) -> Vector3:
 	return linear_velocity + angular_velocity.cross(point - global_position)
+
+func _basic_steering_rotation(delta: float) -> void:
+	var turn_input := Input.get_axis("turn_right", "turn_left") * tire_turn_speed
+
+	if turn_input:
+		$wheels/fl.rotation.y = clampf($wheels/fl.rotation.y + turn_input * delta,
+			deg_to_rad(-tire_max_turn_degrees), deg_to_rad(tire_max_turn_degrees))
+		$wheels/fr.rotation.y = clampf($wheels/fl.rotation.y + turn_input * delta,
+			deg_to_rad(-tire_max_turn_degrees), deg_to_rad(tire_max_turn_degrees))
+	else:
+		$wheels/fl.rotation.y = move_toward($wheels/fl.rotation.y, 0, tire_turn_speed * delta)
+		$wheels/fr.rotation.y = move_toward($wheels/fr.rotation.y, 0, tire_turn_speed * delta)
 
 func _do_single_wheel_acceleration(wheel: Wheel3D) -> void:
 	var forward_dir := -wheel.global_basis.z
 	var vel := forward_dir.dot(linear_velocity)
 
-	wheel.mesh.rotate_x(-vel * get_process_delta_time() * 2 * PI * wheel.wheel_radius)
+	wheel.mesh.rotate_x((-vel * get_process_delta_time()) / wheel.wheel_radius)
 
 	if wheel.is_colliding():
 		var contact := wheel.mesh.global_position
@@ -36,13 +60,39 @@ func _do_single_wheel_acceleration(wheel: Wheel3D) -> void:
 
 			var force_vector := forward_dir * acceleration * motor_input * ac
 			apply_force(force_vector, force_pos)
-			DebugDraw3D.draw_arrow_ray(contact, force_vector / mass, 1, Color.GREEN_YELLOW, 0.1)
-		elif abs(vel) > 0.15 and not motor_input:
-			var drag_force_vector = global_basis.z * deceleration * signf(vel)
-			apply_force(drag_force_vector, force_pos)
-			DebugDraw3D.draw_arrow_ray(contact, drag_force_vector / mass, 1, Color.PURPLE, 0.1)
+			DebugDraw3D.draw_arrow_ray(contact, force_vector / mass, 1, Color.HOT_PINK, 0.1)
 
-# calculate & apply suspension forces
+func _do_single_wheel_traction(wheel: Wheel3D) -> void:
+	if not wheel.is_colliding(): return
+
+	var steer_side_dir := wheel.global_basis.x
+	var tire_vel := _get_point_velocity(wheel.mesh.global_position)
+	var steering_x_vel := steer_side_dir.dot(tire_vel)
+
+	var grip_factor := absf(steering_x_vel / tire_vel.length())
+	var x_traction := wheel.grip_curve.sample_baked(grip_factor)
+
+	if not handbrake and grip_factor < 0.2:
+		is_slipping = false
+
+	if handbrake:
+		x_traction = 0.1
+	elif is_slipping:
+		x_traction = 0.1
+
+	var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+	var x_force := -steer_side_dir * steering_x_vel * x_traction * ((mass * gravity) / 4.0)
+
+	var f_vel := -wheel.global_basis.z.dot(tire_vel)
+	var z_traction := 0.05
+	var z_force := wheel.global_basis.z * f_vel * z_traction * ((mass * gravity) / 4.0)
+
+	var force_pos := wheel.mesh.global_position - global_position
+	apply_force(x_force, force_pos)
+	apply_force(z_force, force_pos)
+	DebugDraw3D.draw_arrow_ray(wheel.mesh.global_position, x_force / mass, 1, Color.ORANGE, 0.1)
+	DebugDraw3D.draw_arrow_ray(wheel.mesh.global_position, z_force / mass, 1, Color.BLUE, 0.1)
+
 func _do_single_wheel_suspension(wheel: Wheel3D) -> void:
 	if wheel.is_colliding():
 		# ensure that the raycast will only collide when 
--- wheel.gd
+++ wheel.gd
@@ -7,5 +7,6 @@ class_name Wheel3D
 @export var over_extend := 0.0 # used to control how much the wheel "sticks" to the ground
 @export var wheel_radius := 0.4
 @export var is_motor := false
+@export var grip_curve : Curve
 
 @onready var mesh: Node3D = get_child(0)
