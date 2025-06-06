0820f8159d0968a4d2d0034434ca655c4cad92e1
Misteki <134941471+mistekii@users.noreply.github.com>
2025-06-06 02:15:23 +0100

    Create Wheel3D class

---
+++ wheel.gd
@@ -0,0 +1,10 @@
+extends RayCast3D
+class_name Wheel3D
+
+@export var spring_strength := 100.0
+@export var spring_damping := 2.0
+@export var rest_dist := 0.5
+@export var over_extend := 0.0 # used to control how much the wheel "sticks" to the ground
+@export var wheel_radius := 0.4
+
+@onready var mesh: Node3D = get_child(0)
